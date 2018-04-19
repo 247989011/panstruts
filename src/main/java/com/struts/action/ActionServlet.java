9347cc2c57ddfc1df1a0a4ca8a74e8bb25faff6f
panqq <247989011@qq.com>
2018-04-19 14:18:36 +0800

    first commit

---
+++ src/main/java/com/struts/action/ActionServlet.java
@@ -0,0 +1,43 @@
+package com.struts.action;
+
+import java.io.IOException;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class ActionServlet
+ */
+public class ActionServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+    /**
+     * Default constructor. 
+     */
+    public ActionServlet() {
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		String name = request.getParameter("text");
+		request.setAttribute("x", name);
+		//获得转发器对象
+		RequestDispatcher dis = request.getRequestDispatcher("view/MyJsp.jsp");
+		dis.forward(request, response);
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
